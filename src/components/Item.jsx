08c0ffd0d08b61c863318057a8dd6a792ac672e9
Franqito73 <franco.picini@hotmail.com>
2025-02-14 00:52:39 -0300

    agregando componentes y fw

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -1,15 +1,21 @@
 import React from 'react'
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import Col from 'react-bootstrap/Col';
 
 function Item({producto}) {
   return (
-    <>
-        <div className='chichilo'>
-          <p>{producto.title}</p>
-          <p>${producto.price}</p>
-          <p>Stock:{producto.stock}</p>
-        </div>
-    </>
-  )
+    <Col md={6} lg={4}>
+        <Card style={{ width: '18rem' }}>
+            <Card.Img variant="top" src={producto.thumbnail}/>
+            <Card.Body>
+                <Card.Title>{producto.title}</Card.Title>
+                <Card.Text>{producto.description}</Card.Text>
+                <Button variant="primary">Ver Más</Button>
+            </Card.Body>
+        </Card>
+    </Col>
+    )
 }
 
 export default Item
--- src/components/ItemList.jsx
+++ src/components/ItemList.jsx
@@ -1,14 +1,17 @@
 import React from 'react'
 import Item from './Item'
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
 
 function ItemList({items}) {
   return (
-    <div>
-        {items.map(producto => (
-        <Item key={producto.id} producto={producto}/>
-      ))}
-    </div>
-  )
+    <Container>
+        <Row>
+       {items.map(producto => <Item key={producto.id} producto={producto}/>)}
+        </Row>
+    </Container>
+
+    )
 }
 
 export default ItemList
